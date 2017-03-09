edd8e283de903e10c6caf783d0cd23ba6b22e260
silver6wings <silver6wings@gmail.com>
2017-03-09 17:57:12 +0800

    First commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+workspace 'SilverAFProtobuffer.xcodeproj'
+project 'SilverAFProtobuffer.xcodeproj'
+
+target 'SilverAFProtobuffer' do
+platform :ios, '8.0'
+project 'SilverAFProtobuffer.xcodeproj'
+
+    pod 'GoogleProtobuf', '~> 2.6.1'
+    #
+
+end
+
---
+++ SilverAFProtobuffer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SilverAFProtobuffer
+//
+//  Created by JUNCHAO on 2017/3/9.
+//  Copyright © 2017年 silver6wings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
